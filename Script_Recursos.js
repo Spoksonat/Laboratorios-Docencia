73f5f8f468d5244e61ff73a5af4dc8c90d28dc19
Spoksonat <40476145+Spoksonat@users.noreply.github.com>
2020-08-11 22:09:10 -0500

    Add files via upload

---
+++ Script_Recursos.js
@@ -0,0 +1,7 @@
+function encender_fondo(id){
+	document.getElementById(id).style.background = "#b3c6ff";
+}
+
+function apagar_fondo(id){
+	document.getElementById(id).style.background = "#ffffff";
+}
